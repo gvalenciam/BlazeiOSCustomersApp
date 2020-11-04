5942442408017607876d5cae85c0d3ddca525e13
gvalenciam <cita@utec.edu.pe>
2020-11-04 16:31:00 -0500

    Initial Commit

---
+++ BlazeTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlazeTest
+//
+//  Created by Gerardo Valencia on 11/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
